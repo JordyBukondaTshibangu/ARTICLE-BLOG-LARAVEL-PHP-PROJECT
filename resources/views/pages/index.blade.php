427d0840585c20c0274a9279aa56f780fe04584e
Code4kongo <65186901+Code4kongo@users.noreply.github.com>
2020-08-14 19:44:16 +0200

    initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+    @section('content')
+        <div class="jumbotron">
+            <h1>{{ $title }}</h1>
+            <p class="lead"> This is a laravel application from a course.</p>
+            <a class="btn btn-lg btn-primary btn-lg" href="/login" role="button">Login</a>
+            <a class="btn btn-lg btn-success" href="/register" role="button">Register &raquo;</a>
+        </div>
+    @endsection
+    
+
+
+    
